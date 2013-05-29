89d956d3c9d2a74f40fa1b2bd5ae0f1abd39c5f2
christian jeria <cjeria@me.com>
2013-05-28 18:26:03 -0700

    commented out rack deflator

--- rack_try_static.rb
+++ rack_try_static.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require 'rubygems'
 
 module Rack
   class TryStatic
@@ -22,15 +23,15 @@ module Rack
   end
 end
 
-use Rack::Deflater
-use Rack::TryStatic, :root => "build", :urls => %w[/], :try => ['.html', 'index.html', '/index.html']
+# use Rack::Deflater
+# use Rack::TryStatic, :root => "build", :urls => %w[/], :try => ['.html', 'index.html', '/index.html']
 
-# Run your own Rack app here or use this one to serve 404 messages:
-run lambda{ |env|
-  not_found_page = File.expand_path("../build/404.html", __FILE__)
-  if File.exist?(not_found_page)
-    [ 404, { 'Content-Type'  => 'text/html'}, [File.read(not_found_page)] ]
-  else
-    [ 404, { 'Content-Type'  => 'text/html' }, ['404 - page not found'] ]
-  end
-}
+# # Run your own Rack app here or use this one to serve 404 messages:
+# run lambda{ |env|
+#   not_found_page = File.expand_path("../build/404.html", __FILE__)
+#   if File.exist?(not_found_page)
+#     [ 404, { 'Content-Type'  => 'text/html'}, [File.read(not_found_page)] ]
+#   else
+#     [ 404, { 'Content-Type'  => 'text/html' }, ['404 - page not found'] ]
+#   end
+# }
